696fbd606e099624186d522cfe0dc866061f7719
Shirkit <shirkit@gmail.com>
2020-05-23 15:57:32 -0300

    Fixed hotkey error

--- data.lua
+++ data.lua
@@ -3,6 +3,6 @@ data:extend({
 	  type = "custom-input",
 	  name = "additional-paste-settings-hotkey",
 	  key_sequence = "SHIFT + mouse-button-1",
-	  consuming = "script-only"
+	  consuming = "none"
 	}
 })
